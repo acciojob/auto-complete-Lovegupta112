e383b981132819726a159b8a0d876605cee7e999
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-07-23 18:52:35 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+   const [search,setSearch]=useState('');
+   const [list,setList]=useState(fruits);
+   
+    function find(e){
+      setSearch(e.target.value);
+      setList(fruits.filter((fruit)=>{
+            return fruit.includes(e.target.value);
+      }))
+    }
+
   return (
     <div>
         {/* Do not remove the main div */}
+       <h1>Search item</h1>
+       <input type="text" onChange={(e)=>find(e)} value={search} />
+       <ul>
+       {list.map((fruit,index)=>{
+          return <li key={index}>{fruit}</li>
+        })}
+       </ul>
+       
     </div>
   )
 }
